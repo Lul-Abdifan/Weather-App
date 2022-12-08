7b9180b487db00f73a8f81522aaaa3cbe5ce0701
Lul-Abdifan <tasanbannu@gmail.com>
2022-12-08 10:42:50 +0300

    Add clear function to make input fields after done

--- script.js
+++ script.js
@@ -11,26 +11,26 @@ fetchWeather:function(city)
 .then((datas)=>this.showOnScreen(datas)) 
 
 },
-// showOnScreen:function(datas)
-// {
-//     const {name}=datas;
-//     const {description}=datas.weather[0]
-//     const {temp_min,temp_max,humidity}=datas.main;
-//     const{speed}=datas.wind;
-//     console.log(name);
-//     console.log(description);
-//     console.log(temp_max,temp_min,humidity);
-//     console.log(speed);
-//     document.getElementById('country').innerHTML="Weather in " + name;
-//     document.getElementById('description').innerHTML=description;
-//     document.getElementById('max_temp').innerHTML="max_temp :" + `br`+ temp_max + " *C";
-//     document.getElementById('min_temp').innerHTML="min_temp :" + temp_min + " *C" ;
-//     document.getElementById('humidity').innerHTML="Humidity :" + humidity + " %";
-//     document.getElementById('speed').innerHTML="Speed :" + speed + " km/hr";
+showOnScreen:function(datas)
+{
+    const {name}=datas;
+    const {description}=datas.weather[0]
+    const {temp_min,temp_max,humidity}=datas.main;
+    const{speed}=datas.wind;
+    console.log(name);
+    console.log(description);
+    console.log(temp_max,temp_min,humidity);
+    console.log(speed);
+    document.getElementById('country').innerHTML="Weather in " + name;
+    document.getElementById('description').innerHTML=description;
+    document.getElementById('max_temp').innerHTML="max_temp :" + `br`+ temp_max + " *C";
+    document.getElementById('min_temp').innerHTML="min_temp :" + temp_min + " *C" ;
+    document.getElementById('humidity').innerHTML="Humidity :" + humidity + " %";
+    document.getElementById('speed').innerHTML="Speed :" + speed + " km/hr";
 
 
-// },
-//add clear input functionality
+},
+
 clearInput:function()
 {
     document.querySelector(".weather-input").value=" ";
